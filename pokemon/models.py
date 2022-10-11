0379c36e6cfd491410e3825b3f4f0c6ae3448991
Engiineeer <ntpzlwo@gmail.com>
2022-10-11 19:40:09 +0300

    model done

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,25 @@
+from email.policy import default
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+
+class Pokemon(models.Model):
+    class PokemonType(models.TextChoices):
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+
+    name = models.CharField(max_length=30)
+    type = models.CharField(
+        max_length=10, choices=PokemonType.choices, default="GHOST")
+    hp = models.PositiveIntegerField(default=10)
+    active = models.BooleanField(default=True)
+    name_fr = models.CharField(max_length=30, default="")
+    name_ar = models.CharField(max_length=30, default="")
+    name_jp = models.CharField(max_length=30, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
